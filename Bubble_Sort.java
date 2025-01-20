179f068c7ff356c19c7969ce9e343d24b80bd3de
Shashi0704 <pandeysha2022@gmail>
2025-01-20 13:55:08 +0530

    bubble sort code

--- Bubble_Sort.java
+++ Bubble_Sort.java
@@ -1,1 +1,27 @@
+package Sorting_Java;
 
+public class Bubble_Sort {
+    public static void bubble(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                }
+            }
+        }
+        System.out.print("Sorted array is");
+        for (int i = 0; i < arr.length - 1; i++) {
+            System.out.print("\t" + arr[i]);
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 4, 5, 3, 6, 2 };
+        bubble(arr);
+    }
+}
